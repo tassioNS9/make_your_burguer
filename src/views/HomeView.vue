20a12f8f6483506fa39ce9c588240025b445d530
tassions@hotmail.com <tassions@hotmail.com>
2023-07-09 10:53:52 -0300

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <Banner />
+  <div class="main-container">
+    <h1>Monte o seu Hamburguer:</h1>
+    <BurgerForm/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Banner from "@/components/Banner.vue";
+import BurgerForm from "@/components/BurgerForm.vue"
+export default {
+  name: "HomeView",
+  components: {
+    Banner,
+    BurgerForm
+  },
+};
+</script>
---
+++ src/views/Requests.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="main-container">
+    <h1>Gerenciar Pedidos</h1>
+    <Dashboard/>
+  </div>
+</template>
+
+
+<script>
+import Dashboard from '@/components/Dashboard.vue';
+// @ is an alias to /src
+
+
+export default {
+  name: 'Requests',
+  components:{
+    Dashboard
+  }
+}
+</script>
